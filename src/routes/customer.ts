07c2f11142d1cf9e49d879057ab525ccdcc70ffd
Punam <punamkumari399@gmail.com>
2024-07-21 22:31:39 +0530

    implemented loan calculation, middleware

--- src/routes/customer.ts
+++ src/routes/customer.ts
@@ -2,22 +2,55 @@ import express from 'express'
 import Customer from '../model/customer'
 import { body, validationResult } from "express-validator";
 import { nanoid } from "nanoid"
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 const router = express.Router()
 
+const JWT_SECRET = "secret"
 
 
 
 // create new customer
 async function createCustomer(req: express.Request, res: express.Response) {
-    try {
 
-        const customer = await Customer.create({
+    let success = false
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res
+            .status(400)
+            .json({ errors: errors.array() });
+    }
+
+    try {
+        let customer = await Customer.findOne({ email: req.body.email })
+
+        if (customer) {
+            return res.status(400).json({
+                success, error: "sorry a user with this email already exists"
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const secPass = await bcrypt.hashSync(req.body.password, salt)
+
+        customer = await Customer.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         })
+
+        const data = {
+            customer: {
+                id: customer._id
+            }
+        }
+
+        const authtoken = jwt.sign(data, JWT_SECRET)
+        success = true
+
         await customer.save()
         console.log(customer)
+        res.json({ success, authtoken })
         res.json(customer)
         res.send("customer created successfully")
     } catch (error) {
@@ -26,6 +59,8 @@ async function createCustomer(req: express.Request, res: express.Response) {
     }
 }
 
+
+// registering 
 router.post('/register',
     [
         body("name").notEmpty().withMessage("Name is Required"),
@@ -38,7 +73,64 @@ router.post('/register',
 )
 
 
-// get the customer details by justentering the id
+// login 
+
+router.post(
+    "/login",
+    [
+        body("email", "Enter a valid email").isEmail(),
+
+        body("password", "Enter password of minimum length 5").exists({ min: 6 }),
+    ],
+    async (req, res) => {
+        let success = false;
+        // finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        // console.log(req.body);
+
+        const { email, password } = req.body;
+
+        try {
+            // if (
+            //   email === config.defaultEmail &&
+            //   password === config.defaultPassword
+            // ) {
+            let customer = await Customer.findOne({ email });
+            // console.log({ user });
+            if (!customer) {
+                success = false;
+                return res
+                    .status(400)
+                    .json({ success, error: "Please login with correct credentials" });
+            }
+
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if (!passwordCompare) {
+                success = false;
+                return res
+                    .status(400)
+                    .json({ error: "Please login with correct credentials" });
+            }
+            const data = {
+                customer: {
+                    id: customer._id,
+                },
+            };
+
+            const authtoken = jwt.sign(data, JWT_SECRET);
+            success = true;
+
+            res.json({ success, authtoken });
+            // }
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal error occurred");
+        }
+    }
+);
 
 
 
